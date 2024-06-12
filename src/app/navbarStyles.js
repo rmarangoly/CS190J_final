055ec440ee94d7effa2e59c911dd61942e863a2f
JosephLiCodes <69280155+JosephLiCodes@users.noreply.github.com>
2024-06-12 16:58:08 -0700

    edit home page etc

---
+++ src/app/navbarStyles.js
@@ -0,0 +1,23 @@
+// src/app/navbarStyles.js
+export const navStyle = {
+    listStyleType: 'none',
+    display: 'flex',
+    justifyContent: 'space-around',
+    padding: '10px 20px',
+    margin: 0,
+    backgroundColor: '#333',
+    color: 'white',
+    width: '100%',
+    height: '50px',  // Ensure the height is defined
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    zIndex: 1000,
+  };
+  
+  export const navLinkStyle = {
+    color: 'white',
+    textDecoration: 'none',
+    padding: '10px',
+  };
+  
