39c1fd55eee813f0fad91cb3b76ebdfcd2dbc986
JosephLiCodes <69280155+JosephLiCodes@users.noreply.github.com>
2024-06-14 19:57:47 -0700

    add comments for documentation

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -1,9 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+/**
+ * @title Marketplace
+ * @dev A smart contract for managing items in a marketplace
+ */
 contract Marketplace {
     struct Item {
-
         uint256 id;
         string name;
         address owner;
@@ -13,16 +16,25 @@ contract Marketplace {
 
     mapping(uint256 => Item) public items;
     uint256 public itemCount;
-
+    
     event ItemListed(uint256 id, string name, address owner, uint256 price);
     event ItemBought(uint256 id, address newOwner, uint256 price);
 
+    /**
+     * @dev List an item for sale
+     * @param name The name of the item
+     * @param price The price of the item
+     */
     function listItem(string memory name, uint256 price) public {
         itemCount++;
         items[itemCount] = Item(itemCount, name, msg.sender, price, true);
         emit ItemListed(itemCount, name, msg.sender, price);
     }
 
+    /**
+     * @dev Buy an item from the marketplace
+     * @param id The ID of the item to buy
+     */
     function buyItem(uint256 id) public payable {
         Item storage item = items[id];
         require(item.listed, "Item not listed for sale");
@@ -36,10 +48,18 @@ contract Marketplace {
         emit ItemBought(id, msg.sender, item.price);
     }
 
+    /**
+     * @dev Get the total count of items in the marketplace
+     * @return The total count of items
+     */
     function getItemCount() public view returns (uint256) {
         return itemCount;
     }
 
+    /**
+     * @dev Relist an item in the marketplace
+     * @param itemId The ID of the item to relist
+     */
     function relistItem(uint256 itemId) public {
         Item storage item = items[itemId];
         require(item.owner == msg.sender, "You are not the owner of this item");
